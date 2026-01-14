6a7b843b54d815994f0804cf3ea880ff8b7444e2
AidenKielby <131564693+AidenKielby@users.noreply.github.com>
2026-01-14 12:20:56 -0500

    Change pet image from alien to dog

--- src/components/pet.tsx
+++ src/components/pet.tsx
@@ -8,7 +8,7 @@ type PetProps = {
 
 const Pet: React.FC<PetProps> = ({ stage }) => {
   let color = stage === "Baby" ? "bg-pink-400" : stage === "Teen" ? "bg-purple-400" : "bg-blue-400";
-  const petImage = "/alien.png"; // place your image at public/pet.png
+  const petImage = "/dog.png"; // place your image at public/pet.png
 
   return (
     <motion.div
